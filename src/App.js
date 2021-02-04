6336962772aa639ebf990afccdd9b7a147c78301
JakeRSA <jakenudels@gmail.com>
2021-02-04 18:23:32 +0200

    implemented Spotify implicit auth grant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import './App.css';
-import StepScreen from './components/StepScreen/StepScreen';
+import "./App.css";
+import { useState, useEffect } from "react";
+import StepScreen from "./components/StepScreen/StepScreen";
+import axios from "axios";
 
 function App() {
+  const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URL } = process.env;
+  const [step, setStep] = useState(1);
+  const [token, setToken] = useState(undefined);
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    const hash = window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce(function (initial, item) {
+        if (item) {
+          var parts = item.split("=");
+          initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+      }, {});
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      axios
+        .get("https://api.spotify.com/v1/me", {
+          headers: { Authorization: 'Bearer ' + _token },
+        })
+        .then((res) => {
+          setUser(res.data)
+          setStep(2);
+        });
+    }
+  }, []);
+
+  const getSpotifyAuth = () => {
+    const authEndpoint = "https://accounts.spotify.com/authorize";
+    const scopes = ["user-library-modify user-read-email"];
+    // If there is no token, redirect to Spotify authorization
+    if (!token) {
+      window.location =
+        `${authEndpoint}?` +
+        `client_id=${REACT_APP_CLIENT_ID}&` +
+        `redirect_uri=${REACT_APP_REDIRECT_URL}&` +
+        `scope=${encodeURIComponent(scopes)}&` +
+        `response_type=token`;
+    }
+  };
+  console.log(token);
   return (
     <div className="App">
-      <StepScreen />
+      <StepScreen
+        step={step}
+        getSpotifyAuth={() => {
+          getSpotifyAuth();
+        }}
+      />
     </div>
   );
 }
--- src/components/StepScreen/StepScreen.js
+++ src/components/StepScreen/StepScreen.js
@@ -5,15 +5,41 @@ import spotifyIcon from "../../spotify.png";
 import loadIcon from "../../load.png";
 import runIcon from "../../run.png";
 
-function StepScreen() {
+function StepScreen(props) {
+  let icon, heading, subHeading;
+  switch (props.step) {
+    case 1:
+      icon = spotifyIcon;
+      heading = "SIGN IN TO SPOTIFY";
+      subHeading="We need permission to add songs to your Spotify library"
+      break;
+    case 2:
+      icon = loadIcon;
+      heading = "SELECT FOLDER"
+      subHeading = "Select a folder to upload to Spotify"
+      break;
+    case 3:
+      icon = runIcon;
+      heading = "RUN SEARCH";
+      subHeading = "Let's find your songs on Spotify"
+      break;
+    default:
+      icon=spotifyIcon;
+  }
+
   return (
     <div>
-      <StepsHeader activeStep={1} />
+      <StepsHeader activeStep={props.step} />
       <div className="main-container">
-          <img className="main-step-btn" alt={"sign in to spotify"} src={spotifyIcon} />
-        <h1 className="btn-action-text">SIGN IN TO SPOTIFY</h1>
+        <img
+          className="main-step-btn"
+          alt={"sign in to spotify"}
+          src={icon}
+          onClick={() => {props.getSpotifyAuth()}}
+        />
+        <h1 className="btn-action-text">{heading}</h1>
         <h4 className="btn-action-subtext">
-          We need permission to add to your Spotify library
+          {subHeading}
         </h4>
       </div>
     </div>
